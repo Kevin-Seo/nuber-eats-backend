a8761451b01b475ab27d80d495c25cb72163fd65
Kevin Youngho Seo <keviny.seo@gmail.com>
2021-01-28 23:22:02 +0900

    #4.4

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,17 @@
-import { Field, InputType, ObjectType } from "@nestjs/graphql";
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
 import { type } from "os";
 import { CoreEntity } from "src/common/entities/core.entity";
 import { Column, Entity } from "typeorm";
 
-type UserRole = 'client' | 'owner' | 'delivery';
+// TypeScript Enum 타입 만들기
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
+
+// GraphQL 에 Enum 만들기
+registerEnumType(UserRole, { name: 'UserRole' });
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -16,8 +24,10 @@ export class User extends CoreEntity {
   @Column()
   @Field(type => String)
   password: string;
-
-  @Column()
-  @Field(type => String)
+  
+  @Column(
+    { type: 'enum', enum: UserRole }
+  )
+  @Field(type => UserRole)
   role: UserRole;
 }
