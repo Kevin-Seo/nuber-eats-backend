07184f044b5009e638c15029e6820f2bf70daa89
Youngho Seo <yh.seo@sbs.co.kr>
2021-02-02 18:01:54 +0900

    #5.3

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { JwtService } from './jwt.service';
 
 @Module({})
-export class JwtModule {}
+@Global() // Global Module 이면 따로 Import 해주지 않아도 Service 접근 가능
+export class JwtModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: JwtModule,
+      exports: [JwtService],
+      providers: [JwtService],
+    }
+  }
+}
