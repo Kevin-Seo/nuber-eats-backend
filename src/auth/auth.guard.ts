d8cf8532539c2b482a6f4e840c6f370b10a746a5
Kevin Youngho Seo <keviny.seo@gmail.com>
2021-02-03 21:17:18 +0900

    #5.9

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const user = gqlContext['user'];
+    console.log(user);
+
+    if (!user)
+      return false;
+      
+    return true;
+    // return false 하면 request 를 막고
+    // return true 하면 request 를 통과시킨다.
+  }
+
+}
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from "@nestjs/common";
 import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { AuthGuard } from "src/auth/auth.guard";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
@@ -40,14 +42,6 @@ export class UsersResolver {
   }
 
   @Query(returns => User)
-  me(
-    @Context()
-    context
-  ) {
-    if (!context.user) {
-      return;
-    } else {
-      return context.user;
-    }
-  }
+  @UseGuards(AuthGuard)
+  me() {}
 }
