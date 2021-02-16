c729266545b249bb9e201725ee4bd7465648e2b8
Kevin Youngho Seo <keviny.seo@gmail.com>
2021-02-17 00:03:45 +0900

    #6.7 (HTTP 401 ERROR)

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import got from 'got';
+import * as FormData from 'form-data';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS)
+    private readonly options: MailModuleOptions,
+    // private readonly configService: ConfigService,
+  ) {
+    this.sendEmail('testing', 'test content')
+        .then(_ => console.log('message sent...'))
+        .catch(error => console.log(error));
+  }
+
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}}>`);
+    form.append('to', `keviny.seo@gmail.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+
+    const response = await got(`https://api.mailgun.net/v3/${this.options.domain}}/messages`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${Buffer.from(`api:${this.options.apiKey}`).toString('base64')}`
+      },
+      body: form,
+    });
+
+    console.log(response.body);
+  }
+}
