6843bdc498e811b5515673cc411328f926d48320
Kevin Youngho Seo <keviny.seo@gmail.com>
2021-02-22 21:43:28 +0900

    7.1 UNIT TESTING THE USER SERVICE - Mocking

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -1,5 +1,25 @@
 import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { JwtService } from "src/jwt/jwt.service";
+import { MailService } from "src/mail/mail.service";
+import { User } from "./entities/user.entity";
+import { Verification } from "./entities/verfication.entity";
 import { UsersService } from "./users.service";
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+}
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+}
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+}
 
 describe('UserService', () => {
 
@@ -7,7 +27,20 @@ describe('UserService', () => {
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, {
+        // mock 은 가짜라는 의미. Test 에 필요한 가짜 Repository 와 Service 를 만들어서 provide 해준다.
+        provide: getRepositoryToken(User),
+        useValue: mockRepository,
+      }, {
+        provide: getRepositoryToken(Verification),
+        useValue: mockRepository,
+      }, {
+        provide: JwtService,
+        useValue: mockJwtService,
+      }, {
+        provide: MailService,
+        useValue: mockMailService,
+      }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
