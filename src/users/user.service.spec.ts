673122418e5653675d42c907382d6b8d02aff6fd
Kevin Youngho Seo <keviny.seo@gmail.com>
2021-02-24 20:19:37 +0900

    7.9 UNIT TESTING THE USER SERVICE - findById Test

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -11,6 +11,7 @@ const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
+  findOneOrFail: jest.fn(),
 }); // returns { ~ } 와 같다.
 
 const mockJwtService = {
@@ -173,8 +174,30 @@ describe('UserService', () => {
         token: 'signed-token-baby',
       })
     });
+
+    it('should fail on exception', async () => {
+      usersRepository.findOne.mockRejectedValue(new Error());
+      const result = await service.login(loginArgs);
+      expect(result).toEqual({ ok: false, error: "Can't log user in." });
+    });
   });
-  it.todo('findById');
+
+  describe('findById', () => {
+    const findByIdArgs = {
+      id: 1,
+    };
+    it('should find an existing user', async () => {
+      usersRepository.findOneOrFail.mockResolvedValue(findByIdArgs);
+      const result = await service.findById(1);
+      expect(result).toEqual({ ok: true, user: findByIdArgs });
+    });
+
+    it('should fail if no user is found', async () => {
+      usersRepository.findOneOrFail.mockRejectedValue(new Error());
+      const result = await service.findById(1);
+      expect(result).toEqual({ ok: false, error: 'User Not Found' });
+    });
+  });
   it.todo('editProfile');
   it.todo('verifyEmail');
 })
