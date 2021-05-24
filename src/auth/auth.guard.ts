65a25d516ecc62b9ea87f7995de994d21094e1fa
Kevin Youngho Seo <keviny.seo@gmail.com>
2021-05-25 00:59:05 +0900

    #10 RESTAURANT CRUD - 5. Roles part Two

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,20 +1,29 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { GqlExecutionContext } from "@nestjs/graphql";
 import { Observable } from "rxjs";
+import { User } from "src/users/entities/user.entity";
+import { AllowedRoles } from "./role.decorator";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<AllowedRoles>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user = gqlContext['user'];
-    // console.log(user);
+    const user: User = gqlContext['user'];
 
     if (!user)
       return false;
+
+    if (roles.includes('Any'))
+      return true;
       
-    return true;
+    return roles.includes(user.role);
     // return false 하면 request 를 막고
     // return true 하면 request 를 통과시킨다.
   }
-
 }
