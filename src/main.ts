d459984efed54b1c4a0c20b106af8a762ebcbcb1
Kevin Youngho Seo <keviny.seo@gmail.com>
2021-05-26 01:41:37 +0900

    PORT

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe()
-  )
+  app.useGlobalPipes(new ValidationPipe());
   // app.use(jwtMiddleware); // functional middleware 만 가능
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
